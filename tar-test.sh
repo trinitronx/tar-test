ff4fd4c3bdfd32734b0c22b057aba21df5a1e83f
James Cuzella <james.cuzella@lyraphase.com>
2012-03-16 11:00:21 -0600

    Automatically set strip_components based on APP_EXTRACT_DIR

--- tar-test.sh
+++ tar-test.sh
@@ -3,21 +3,35 @@
 num_fxns=6
 test_log=/tmp/tar-tests/tar-test.log
 
+## Function to auto-detect the number of dirs to strip
+set_strip_components() {
+	if [ -z "${APP_EXTRACT_DIR}" ]; then
+		strip_components="--strip-components=0"
+	else
+		local dirs_to_strip=( $(echo "$APP_EXTRACT_DIR" | tr '/' '\n' ) )
+		strip_components="--strip-components=${#dirs_to_strip[*]}"
+	fi
+}
+
 ## Various methods to try and bypass the weird bash issue
 fxn1() {
+	set_strip_components
 	tar $TAR_PARMS $strip_components --wildcards --overwrite -xzf "$FOUND_APP_ARCHIVE" "$APP_EXTRACT_DIR" 1>&2 2>> $test_log
 	export result=$?
 }
 fxn2() {
+	set_strip_components
 	tar $TAR_PARMS $strip_components --wildcards --overwrite -xzf "$FOUND_APP_ARCHIVE" $APP_EXTRACT_DIR 1>&2 2>> $test_log
 	export result=$?
 }
 fxn3() {
+	set_strip_components
 	[ -n "$APP_EXTRACT_DIR" ] && local app_extract_dir=$(echo -n "\"${APP_EXTRACT_DIR}\"")
 	tar $TAR_PARMS $strip_components --wildcards --overwrite -xzf "$FOUND_APP_ARCHIVE" $app_extract_dir 1>&2 2>> $test_log
 	export result=$?
 }
 fxn4() {
+	set_strip_components
 	local app_extract_dir="$(echo "$APP_EXTRACT_DIR" | sed -e 's| |\\ |g')"
 	local need_to_eval=$(echo "$APP_EXTRACT_DIR" | grep -c '\\')
 	[ "$need_to_eval" != '0' ] && local EVAL='eval'
@@ -26,6 +40,7 @@ fxn4() {
 }
 
 fxn5() {
+	set_strip_components
 	local app_extract_dir="$(echo "$APP_EXTRACT_DIR" | sed -e 's| |\\ |g')"
 	local need_to_eval=$(echo "$APP_EXTRACT_DIR" | grep -c '\\')
 	[ "$need_to_eval" != '0' ] && local EVAL='eval'
@@ -33,6 +48,7 @@ fxn5() {
 	export result=$?
 }
 fxn6() {
+	set_strip_components
 	tar $TAR_PARMS $strip_components --wildcards --overwrite -xzf "$FOUND_APP_ARCHIVE" ${APP_EXTRACT_DIR:+"$APP_EXTRACT_DIR"} 1>&2 2>> $test_log
 	export result=$?
 }
@@ -60,32 +76,26 @@ mkdir /tmp/tar-tests
 echo -e "\e[1;36mTEST1: NULL string\e[0m" | tee -a $test_log
 TAR_PARMS='-C /tmp/tar-tests'
 FOUND_APP_ARCHIVE=null-test.tar.gz
-strip_components=--strip-components=0
 APP_EXTRACT_DIR=''
 run_tests $num_fxns
 
 echo -e "\e[1;36mTEST2: SPACES\e[0m" | tee -a $test_log
 FOUND_APP_ARCHIVE=spaces-test.tar.gz
 APP_EXTRACT_DIR='spaces test success'
-strip_components=--strip-components=1
 run_tests $num_fxns
 
 echo -e "\e[1;36mTEST3: Wildcards\e[0m" | tee -a $test_log
 FOUND_APP_ARCHIVE=wildcard-test.tar.gz
 APP_EXTRACT_DIR="wildcard-*"
-strip_components=--strip-components=1
 run_tests $num_fxns
 
 echo -e "\e[1;36mTEST4: Multiple directories\e[0m" | tee -a $test_log
 FOUND_APP_ARCHIVE=multiple-directory-test.tar.gz
 APP_EXTRACT_DIR="multiple-directory-test-success/multiple-directory-test-success"
-strip_components=--strip-components=2
 run_tests $num_fxns
 
 echo -e "\e[1;36mTEST5: Multiple directories with spaces test\e[0m" | tee -a $test_log
 APP_EXTRACT_DIR='multiple directory spaces test success/multiple directory spaces test success'
 FOUND_APP_ARCHIVE=multiple-directory-spaces.tar.gz
-strip_components=--strip-components=2
-run_tests $num_fxns
-
+run_tests $num_fxns 
 
